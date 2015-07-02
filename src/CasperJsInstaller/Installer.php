d5b9215e7533c57349179b7ae2b229d386af5577
Jérôme Breton <jbreton@sqli.com>
2015-07-02 10:02:12 +0200

    Rewrite using jakoch/phantomjs-installer strategy

---
+++ src/CasperJsInstaller/Installer.php
@@ -0,0 +1,162 @@
+<?php
+
+/*
+ * This file is part of the "jakoch/phantomjs-installer" package.
+ *
+ * Copyright (c) 2013-2014 Jens-André Koch <jakoch@web.de>
+ *
+ * The content is released under the MIT License. Please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace CasperJsInstaller;
+
+use Composer\Script\Event;
+use Composer\Composer;
+
+use Composer\Package\Package;
+use Composer\Package\RootPackageInterface;
+use Composer\Package\Version\VersionParser;
+
+class Installer
+{
+    const CASPERJS_NAME = 'CasperJS';
+
+    const CASPERJS_TARGETDIR = '/jerome-breton/casperjs';
+
+    /**
+     * Operating system dependend installation of CasperJS
+     */
+    public static function installCasperJS(Event $event)
+    {
+        $composer = $event->getComposer();
+
+        $version = self::getVersion($composer);
+
+        $url = self::getURL($version);
+
+        $binDir = $composer->getConfig()->get('bin-dir');
+
+        // the installation folder depends on the vendor-dir (default prefix is './vendor')
+        $targetDir = $composer->getConfig()->get('vendor-dir') . self::CASPERJS_TARGETDIR;
+
+        // Create Composer In-Memory Package
+
+        $versionParser = new VersionParser();
+        $normVersion = $versionParser->normalize($version);
+
+        $package = new Package(self::CASPERJS_NAME, $normVersion, $version);
+        $package->setTargetDir($targetDir);
+        $package->setInstallationSource('dist');
+        $package->setDistType(pathinfo($url, PATHINFO_EXTENSION) === 'zip' ? 'zip' : 'tar'); // set zip, tarball
+        $package->setDistUrl($url);
+
+        // Download the Archive
+
+        $downloadManager = $composer->getDownloadManager();
+        $downloadManager->download($package, $targetDir, false);
+
+        // Copy only the PhantomJS binary from the installation "target dir" to the "bin" folder
+
+//        self::copyCasperJsBinaryToBinFolder($targetDir, $binDir);
+    }
+
+    /**
+     * Returns the PhantomJS version number.
+     *
+     * Firstly, we search for a version number in the local repository,
+     * secondly, in the root package.
+     * A version specification of "dev-master#<commit-reference>" is disallowed.
+     *
+     * @param Composer $composer
+     * @return string $version Version
+     */
+    public static function getVersion($composer)
+    {
+        $packages = $composer->getRepositoryManager()->getLocalRepository()->getCanonicalPackages();
+
+        foreach($packages as $package) {
+            if($package->getName() === 'jerome-breton/casperjs-installer') {
+                $version = $package->getPrettyVersion();
+            }
+        }
+
+        // version was not found in the local repository, let's take a look at the root package
+        if($version == null) {
+            $version = self::getRequiredVersion($composer->getPackage(), 'jerome-breton/casperjs-installer');
+        }
+
+        if($version == 'dev-master'){
+            return 'master';
+        }
+
+        return $version;
+    }
+
+    /**
+     * Returns the version for the given package either from the "require" or "require-dev" packages array.
+     *
+     * @param RootPackageInterface $package
+     * @param string $packageName
+     * @throws \RuntimeException
+     * @return mixed
+     */
+    public static function getRequiredVersion(RootPackageInterface $package, $packageName = 'jerome-breton/casperjs-installer')
+    {
+        foreach (array($package->getRequires(), $package->getDevRequires()) as $requiredPackages) {
+            if (isset($requiredPackages[$packageName])) {
+                return $requiredPackages[$packageName]->getPrettyConstraint();
+            }
+        }
+        throw new \RuntimeException('Can not determine required version of ' . $packageName);
+    }
+
+    /**
+     * Returns the URL of the PhantomJS distribution for the installing OS.
+     *
+     * @param string $version
+     * @return string Download URL
+     */
+    public static function getURL($version)
+    {
+        return 'https://github.com/n1k0/casperjs/zipball/'.$version;
+    }
+
+    /**
+     * Returns the Operating System.
+     *
+     * @return string OS, e.g. macosx, windows, linux.
+     */
+    public static function getOS()
+    {
+        $uname = strtolower(php_uname());
+
+        if (strpos($uname, "darwin") !== false) {
+            return 'macosx';
+        } elseif (strpos($uname, "win") !== false) {
+            return 'windows';
+        } elseif (strpos($uname, "linux") !== false) {
+            return 'linux';
+        } else {
+            return 'unknown';
+        }
+    }
+
+    /**
+     * Returns the Bit-Size.
+     *
+     * @return string BitSize, e.g. 32, 64.
+     */
+    public static function getBitSize()
+    {
+        if (PHP_INT_SIZE === 4) {
+            return 32;
+        }
+
+        if (PHP_INT_SIZE === 8) {
+            return 64;
+        }
+
+        return PHP_INT_SIZE; // 16-bit?
+    }
+}
